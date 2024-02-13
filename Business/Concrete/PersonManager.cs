903d615416b92d91dec10f893d281d47c91bd81b
Ahmet <Ahmet@FURKAN>
2024-02-14 00:12:14 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using mERNİSServiceReference;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager:IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            //mernis kontrol yapılacak.
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody( person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBrithYear))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+
+
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,82 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //SelamVer("Furkan");
+            //SelamVer();
+
+
+            //int sonuc=   Topla(3,5);
+
+
+            //Diziler-Arrays
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Ahmet";
+            string ogrenci3 = "Sevde";
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Ahmet";
+            ogrenciler[2] = "Sevde";
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            Person person1=new Person();
+            person1.FirstName = "AHMET FURKAN";
+            person1.LastName = "ÇELİK";
+            person1.DateOfBrithYear = 1998;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Furkan";
+
+            foreach (var sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+            List<string> yeniSehierler1 = new List<string> { "Ankara", "İstanbul", "İzmir" };
+            yeniSehierler1.Add("Sakarya");
+            foreach (var sehir in yeniSehierler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+
+
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+        static void SelamVer(string isim="")
+        {
+            Console.WriteLine("Merhaba "+isim);
+        }
+        
+        static int Topla(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: "+sonuc);
+            return sonuc;
+        }
+
+
+    }
+}
